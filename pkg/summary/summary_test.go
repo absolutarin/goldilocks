817cc97a3ce591faded9a25418de0c025b14b717
Mary Goodhart <mary.loves.pry@gmail.com>
2019-10-15 09:55:51 -0600

    Adds currently commented out test for future iteration of refactoring as it went out of scope

--- pkg/summary/summary_test.go
+++ pkg/summary/summary_test.go
@@ -18,18 +18,20 @@ import (
 	"testing"
 
 	"github.com/fairwindsops/goldilocks/pkg/kube"
-	"github.com/fairwindsops/goldilocks/pkg/utils"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 )
 
 func TestRun(t *testing.T) {
-	// this always has to be present when the run func is called now
+	fakeVpaLabels := map[string]string{
+		"kind": "Deployment",
+	}
+
 	kubeClientVPA := kube.GetMockVPAClient()
 
 	fakeVpaListOptions := metav1.ListOptions{
-		LabelSelector: labels.Set(utils.VpaLabels).String(),
+		LabelSelector: labels.Set(fakeVpaLabels).String(),
 	}
 
 	_, errOk := kubeClientVPA.Client.AutoscalingV1beta2().VerticalPodAutoscalers("").List(fakeVpaListOptions)
@@ -37,11 +39,68 @@ func TestRun(t *testing.T) {
 
 	var summary Summary
 
-	got, err := Run(kubeClientVPA, utils.VpaLabels, "true")
+	got, err := Run(kubeClientVPA, fakeVpaLabels, "true")
 	assert.NoError(t, err)
 
 	assert.EqualValues(t, got, summary)
 }
 
-// called on line 41 cmd/summary.go
-// called on line 165 in dashboard.go
+// func TestRunSummary(t *testing.T) {
+// constructSummary should be refactored to pass a client in.
+// This test is commented out for now and
+// Will be refactored/renamed after
+// Some work is done on refactoring functions for better scope
+// 	kubeClientVPA := kube.GetMockVPAClient()
+
+// 	updateMode := v1beta2.UpdateModeOff
+// 	var testVPA = &v1beta2.VerticalPodAutoscaler{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      "test-vpa",
+// 			Labels:    utils.VpaLabels,
+// 			Namespace: "testing",
+// 		},
+// 		Spec: v1beta2.VerticalPodAutoscalerSpec{
+// 			TargetRef: &autoscaling.CrossVersionObjectReference{
+// 				APIVersion: "extensions/v1beta1",
+// 				Kind:       "Deployment",
+// 				Name:       "test-vpa",
+// 			},
+// 			UpdatePolicy: &v1beta2.PodUpdatePolicy{
+// 				UpdateMode: &updateMode,
+// 			},
+// 		},
+// 	}
+// 	var testVPANoLabels = &v1beta2.VerticalPodAutoscaler{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      "test-vpa",
+// 			Namespace: "testing",
+// 		},
+// 		Spec: v1beta2.VerticalPodAutoscalerSpec{
+// 			TargetRef: &autoscaling.CrossVersionObjectReference{
+// 				APIVersion: "extensions/v1beta1",
+// 				Kind:       "Deployment",
+// 				Name:       "test-vpa",
+// 			},
+// 			UpdatePolicy: &v1beta2.PodUpdatePolicy{
+// 				UpdateMode: &updateMode,
+// 			},
+// 		},
+// 	}
+
+// 	_, errOk := kubeClientVPA.Client.AutoscalingV1beta2().VerticalPodAutoscalers("testing").Create(testVPA)
+// 	assert.NoError(t, errOk)
+
+// 	_, errOk2 := kubeClientVPA.Client.AutoscalingV1beta2().VerticalPodAutoscalers("testing").Create(testVPANoLabels)
+// 	assert.NoError(t, errOk2)
+
+// 	var summary = Summary{
+// 		Namespaces: []string{
+// 			"testing",
+// 		},
+// 	}
+
+// 	got, err := Run(kubeClientVPA, utils.VpaLabels, "true")
+// 	assert.NoError(t, err)
+
+// 	assert.EqualValues(t, summary, got)
+// }
